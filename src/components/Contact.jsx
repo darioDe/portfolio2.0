5e172e6c0d06984e367d19111940e57b91cdb2af
Ruben Duarte <rdduarte1811@gmail.com>
2023-01-24 06:50:17 -0300

    add elements to contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
+;
+
+const initialForm = {};
+
+const validationsForm = (form) => {};
 
 const Contact = () => {
+  // FORM CONTROLLER HOOK
+  const  {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validationsForm);
+
   return (
-   <section>
-      <h3> CONTACT ME </h3>
+   <section className='contact-container'>
+      <h3 className='contact-h3'> CONTACT ME </h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name='name' 
+          placeholder='Name' 
+          onBlur={handleBlur} 
+          onChange={handleChange} 
+          value={form.name} 
+          required 
+        />
+        <input 
+          type="email" 
+          name='email' 
+          placeholder='E-mail' 
+          onBlur={handleBlur} 
+          onChange={handleChange} 
+          value={form.email} 
+          required 
+        />
+        <input 
+          type="text" 
+          name='subject' 
+          placeholder='About' 
+          onBlur={handleBlur} 
+          onChange={handleChange} 
+          value={form.subject} 
+          required 
+        />
+        <textarea 
+          name="comments" 
+          cols="50" 
+          rows="5" 
+          placeholder='Read here!'
+          onBlur={handleBlur} 
+          onChange={handleChange} 
+          value={form.comments} 
+          required  
+        > </textarea>
+        <input type="submit" value="Send" />
+
+      </form>
       
    </section>
   )
