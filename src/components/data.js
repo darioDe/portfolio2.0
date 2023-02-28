4dcd8a5b052602c741d6cabda9c575ddfd1117f7
Ruben Duarte <rdduarte1811@gmail.com>
2023-02-28 06:22:50 -0300

    adding clime project, adjusting styles

--- src/components/data.js
+++ src/components/data.js
@@ -1,20 +1,26 @@
 export const works = [
    {
-      title: 'Form-Patients-App',
+      title: 'Form Monitorating Patients',
       despcription: `This is a project developed with HTML, Tailwind CSS, JavaScript and React JS that consists of an application for an operating room. In this application, you can load the details of a patient and their surgery, and you can also edit and delete them.`,
       repository: 'https://github.com/darioDe/Form-Patients-App',
       deploy: 'https://form-patients.netlify.app',
       image: 'images/form-patients.png',
-      tecnologies: ['public/img/icon-html.png', 'public/img/icon-css.png', 'public/img/icon-js.png', 'public/img/icon-react.png',],
       id: 1,
    },
    {
-      title: 'Cost-Planificator-App',
+      title: 'Budget Planificator',
       despcription: `This budget tracking application is made with HTML, CSS, JavaScript, and React JS. Also uses the React Swipeable List and React-Circular-Progressbar libraries. In this app, you can load a budget, add, edit, and delete expenses and have control over them and their details.`,
       repository: 'https://github.com/darioDe/Form-Patients-App',
       deploy: 'https://cost-planificator.netlify.app/',
       image: 'images/cost-planificator.png',
-      tecnologies: ['public/img/icon-html.png', 'public/img/icon-css.png', 'public/img/icon-js.png', 'public/img/icon-react.png',],
       id: 2,
+   },
+   {
+      title: 'City Clime Searcher',
+      despcription: `This budget tracking application is made with HTML, CSS, JavaScript, and React JS. Also uses the React Swipeable List and React-Circular-Progressbar libraries. In this app, you can load a budget, add, edit, and delete expenses and have control over them and their details.`,
+      repository: 'https://github.com/darioDe/Form-Patients-App',
+      deploy: 'https://climetypereact.netlify.app',
+      image: 'images/clime.png',
+      id: 3,
    },
 ];
