1bc57057020abbd350ddb8ff768a283272262618
Ruben Duarte <rdduarte1811@gmail.com>
2023-03-06 16:18:47 -0300

    add description to new project

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,13 @@
 export const works = [
    {
+      title: 'City Clime Searcher',
+      despcription: `This is an application developed with HTML, CSS, TypeScript, and React that allows users to search for weather information by city using the OpenWeatherMap API. It provides up-to-date information on temperature, humidity, and other relevant details. The interface is user-friendly and responsive, adaptable to various devices.`,
+      repository: 'https://github.com/darioDe/Form-Patients-App',
+      deploy: 'https://climetypereact.netlify.app',
+      image: 'images/clime.png',
+      id: 3,
+   },
+   {
       title: 'Form Monitorating Patients',
       despcription: `This is a project developed with HTML, Tailwind CSS, JavaScript and React JS that consists of an application for an operating room. In this application, you can load the details of a patient and their surgery, and you can also edit and delete them.`,
       repository: 'https://github.com/darioDe/Form-Patients-App',
@@ -14,13 +22,5 @@ export const works = [
       deploy: 'https://cost-planificator.netlify.app/',
       image: 'images/cost-planificator.png',
       id: 2,
-   },
-   {
-      title: 'City Clime Searcher',
-      despcription: `This budget tracking application is made with HTML, CSS, JavaScript, and React JS. Also uses the React Swipeable List and React-Circular-Progressbar libraries. In this app, you can load a budget, add, edit, and delete expenses and have control over them and their details.`,
-      repository: 'https://github.com/darioDe/Form-Patients-App',
-      deploy: 'https://climetypereact.netlify.app',
-      image: 'images/clime.png',
-      id: 3,
    },
 ];
